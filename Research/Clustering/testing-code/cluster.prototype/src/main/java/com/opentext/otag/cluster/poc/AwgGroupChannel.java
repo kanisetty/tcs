78b790ddbb5fbf23deae408abbfc0f3f922230fa
Rhys Evans <rhyse@opentext.com>
2015-08-05 10:56:57 -0700

    Cluster POC Research: Finishing touches so I can move things into the Gateway

--- Research/Clustering/testing-code/cluster.prototype/src/main/java/com/opentext/otag/cluster/poc/AwgGroupChannel.java
+++ Research/Clustering/testing-code/cluster.prototype/src/main/java/com/opentext/otag/cluster/poc/AwgGroupChannel.java
@@ -11,37 +11,43 @@ import org.apache.commons.logging.LogFactory;
 
 import java.io.Serializable;
 
+/**
+ * Awg implementation of Apache Tribes org.apache.catalina.tribes.group.GroupChannel
+ * we have fixed the messageRecieved method so that is provides a decent set of
+ * Classloaders for the deserialization of messages. Absurd really.
+ */
 public class AwgGroupChannel extends GroupChannel {
 
-    private static final Log log = LogFactory.getLog(AwgGroupChannel.class);
+    private static final Log LOG = LogFactory.getLog(AwgGroupChannel.class);
 
     @Override
     public void messageReceived(ChannelMessage msg) {
-        if ( msg == null ) return;
+        if (msg == null)
+            return;
+
         try {
-            if ( Logs.MESSAGES.isTraceEnabled() ) {
-                Logs.MESSAGES.trace("GroupChannel - Received msg:" +
+            if (LOG.isTraceEnabled()) {
+                LOG.trace("GroupChannel - Received msg:" +
                         new UniqueId(msg.getUniqueId()) + " at " +
                         new java.sql.Timestamp(System.currentTimeMillis()) + " from " +
                         msg.getAddress().getName());
             }
 
-            Serializable fwd = null;
-
-            if ( (msg.getOptions() & SEND_OPTIONS_BYTE_MESSAGE) == SEND_OPTIONS_BYTE_MESSAGE ) {
+            Serializable fwd;
+            if ((msg.getOptions() & SEND_OPTIONS_BYTE_MESSAGE) == SEND_OPTIONS_BYTE_MESSAGE) {
                 fwd = new ByteMessage(msg.getMessage().getBytes());
             } else {
                 try {
                     fwd = XByteBuffer.deserialize(msg.getMessage().getBytesDirect(), 0,
                             msg.getMessage().getLength(), getClassLoaders());
                 }catch (Exception sx) {
-                    log.error(sm.getString("groupChannel.unable.deserialize", msg),sx);
+                    LOG.error(sm.getString("groupChannel.unable.deserialize", msg), sx);
                     return;
                 }
             }
 
-            if ( Logs.MESSAGES.isTraceEnabled() ) {
-                Logs.MESSAGES.trace("GroupChannel - Receive Message:" +
+            if (Logs.MESSAGES.isTraceEnabled() ) {
+                LOG.debug("GroupChannel - Receive Message:" +
                         new UniqueId(msg.getUniqueId()) + " is " + fwd);
             }
 
@@ -49,40 +55,48 @@ public class AwgGroupChannel extends GroupChannel {
             Member source = msg.getAddress();
             boolean rx = false;
             boolean delivered = false;
-            for ( int i=0; i<channelListeners.size(); i++ ) {
-                ChannelListener channelListener = (ChannelListener)channelListeners.get(i);
+            for (int i = 0; i < channelListeners.size(); i++) {
+                ChannelListener channelListener = (ChannelListener) channelListeners.get(i);
                 if (channelListener != null && channelListener.accept(fwd, source)) {
                     channelListener.messageReceived(fwd, source);
                     delivered = true;
                     //if the message was accepted by an RPC channel, that channel
                     //is responsible for returning the reply, otherwise we send an absence reply
-                    if ( channelListener instanceof RpcChannel) rx = true;
+                    if (channelListener instanceof RpcChannel)
+                        rx = true;
                 }
-            }//for
-            if ((!rx) && (fwd instanceof RpcMessage)) {
+            }
+
+            if (!rx && (fwd instanceof RpcMessage)) {
                 //if we have a message that requires a response,
                 //but none was given, send back an immediate one
                 sendNoRpcChannelReply((RpcMessage)fwd,source);
             }
-            if ( Logs.MESSAGES.isTraceEnabled() ) {
-                Logs.MESSAGES.trace("GroupChannel delivered[" + delivered + "] id:" +
+
+            if (LOG.isTraceEnabled() ) {
+                LOG.trace("GroupChannel delivered[" + delivered + "] id:" +
                         new UniqueId(msg.getUniqueId()));
             }
 
-        } catch ( Exception x ) {
+        } catch (Exception e) {
             //this could be the channel listener throwing an exception, we should log it
             //as a warning.
-            if ( log.isWarnEnabled() ) log.warn(sm.getString("groupChannel.receiving.error"),x);
-            throw new RemoteProcessException("Exception:"+x.getMessage(),x);
+            LOG.warn(sm.getString("groupChannel.receiving.error"), e);
+            throw new RemoteProcessException("Exception:"+ e.getMessage(),e);
         }
     }
 
-    private ClassLoader[] getClassLoaders()
-    {
+    /**
+     * Get the classloaders that are available.
+     *
+     * @return classloader array
+     */
+    private ClassLoader[] getClassLoaders() {
         return new ClassLoader[] {
                 Thread.currentThread().getContextClassLoader(),
                 getClass().getClassLoader(),
                 ClassLoader.getSystemClassLoader()
         };
     }
+
 }
