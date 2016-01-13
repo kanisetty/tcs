d2854abc375c9c14f9d9a9ed1528b1a335ec7986
jinshanyang <jyang@opentext.com>
2016-01-13 10:18:47 -0500

    change API used for image operation

--- otsync.bundles/dcs/src/main/java/com/opentext/otsync/dcs/utils/ImageUtils.java
+++ otsync.bundles/dcs/src/main/java/com/opentext/otsync/dcs/utils/ImageUtils.java
@@ -1,40 +1,35 @@
 package com.opentext.otsync.dcs.utils;
 
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
-
-import com.sun.image.codec.jpeg.JPEGCodec;
-import com.sun.image.codec.jpeg.JPEGEncodeParam;
-import com.sun.image.codec.jpeg.JPEGImageEncoder;
-
 public class ImageUtils {
     private static boolean saveBufferedImageAsJPEG(BufferedImage image, String imagePath) {
-        FileOutputStream outputStream;
-        boolean success = true;
-
         try {
-            outputStream = new FileOutputStream(imagePath);
+            FileOutputStream outputStream = new FileOutputStream(imagePath);
+            ImageWriter jpgWriter = ImageIO.getImageWritersByFormatName("jpg").next();
+            ImageWriteParam jpgWriteParam = jpgWriter.getDefaultWriteParam();
+            jpgWriteParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+            jpgWriteParam.setCompressionQuality(0.93f);
 
-            JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(outputStream);
-            JPEGEncodeParam param = encoder.getDefaultJPEGEncodeParam(image);
-
-            param.setQuality(0.93f, true);
-
-            encoder.setJPEGEncodeParam(param);
-            encoder.encode(image);
+            jpgWriter.setOutput(outputStream);
+            IIOImage outputImage = new IIOImage(image, null, null);
+            jpgWriter.write(null, outputImage, jpgWriteParam);
+            jpgWriter.dispose();
 
             outputStream.close();
         } catch (Exception ex) {
-            success = false;
+            return false;
         }
 
-        return success;
+        return true;
     }
 
     public static void scaleDocImageToProperSize(File imageToScale, int maxFileSize) throws IOException {
