41ae0040f994d9e078c2dfdb144788a2b46a7ad2
Jason Ibrahim <jibrahim@opentext.com>
2016-09-21 10:49:46 -0700

    styling updates

--- otsync.html5/workflow-attachments-component/src/app/js/providers/content-provider.js
+++ otsync.html5/workflow-attachments-component/src/app/js/providers/content-provider.js
@@ -11,28 +11,34 @@ function ContentProvider($q, authService, $http) {
 
     this.getEWSRoot = function getEWSRoot() {
         var deferred = $q.defer();
-        var url = authService.gatewayUrl() + '/content/v5/properties';
-        $http.get(url).then(function (res) {
-            deferred.resolve(res.data.enterpriseWorkspaceRoot);
-        }, deferred.reject);
+        authService.initialize().then(function () {
+            var url = authService.gatewayUrl() + '/content/v5/properties';
+            $http.get(url).then(function (res) {
+                deferred.resolve(res.data.enterpriseWorkspaceRoot);
+            }, deferred.reject);
+        });
         return deferred.promise;
     };
 
     this.getPWSRoot = function getPWSRoot() {
         var deferred = $q.defer();
-        var url = authService.gatewayUrl() + '/content/v5/properties';
-        $http.get(url).then(function (res) {
-            deferred.resolve(res.data.personalWorkspaceRoot);
-        }, deferred.reject);
+        authService.initialize().then(function () {
+            var url = authService.gatewayUrl() + '/content/v5/properties';
+            $http.get(url).then(function (res) {
+                deferred.resolve(res.data.personalWorkspaceRoot);
+            }, deferred.reject);
+        });
         return deferred.promise;
     };
 
     this.getChildren = function getChildren(parentId) {
         var deferred = $q.defer();
-        var url = authService.gatewayUrl() + '/content/v5/nodes/' + parentId + '/children';
-        $http.get(url).then(function (res) {
-            deferred.resolve(res.data.contents);
-        }, deferred.reject);
+        authService.initialize().then(function () {
+            var url = authService.gatewayUrl() + '/content/v5/nodes/' + parentId + '/children';
+            $http.get(url).then(function (res) {
+                deferred.resolve(res.data.contents);
+            }, deferred.reject);
+        });
         return deferred.promise;
     };
 
